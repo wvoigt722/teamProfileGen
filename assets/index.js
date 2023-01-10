8ed61afad19f995cdcdb1da71e1a8e0f749e9fc2
William Voigt <wvoigt@Williams-MacBook-Air.local>
2023-01-09 16:04:08 -0800

    put promts into functions and started creating classes

--- assets/index.js
+++ assets/index.js
@@ -1,78 +1,114 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generatehtml = require("./generatehtml");
+const internClass = require("./internClass");
+const managerClass = require("./managerClass");
+const engineerClass = require("./engineerClass");
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is the team managers name?",
-    name: "managerName",
-  },
-  {
-    type: "input",
-    message: "What is the employee ID?",
-    name: "empID",
-  },
-  {
-    type: "input",
-    message: "What is the email address?",
-    name: "emailId",
-  },
-  {
-    type: "input",
-    message: "What is the office number?",
-    name: "officeNum",
-  },
-  {
-    type: "list",
-    message: "Add an employee, or exit.",
-    name: "addEmp",
-    choices: ["Engineer", "Intern", "Exit"],
-  },
+const init = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team managers name?",
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: "What is the employee ID?",
+        name: "empID",
+      },
+      {
+        type: "input",
+        message: "What is the email address?",
+        name: "emailId",
+      },
+      {
+        type: "input",
+        message: "What is the office number?",
+        name: "officeNum",
+      },
+    ])
 
-  //if selected exit then exit the promts
+    .then(data);
+};
 
-  //if selected engineer is selected from choices then do the following
+const whatNext = function () {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "addEmp",
+      choices: ["Add an Engineer?", "Add an Intern?", "Exit?"],
+    })
+    .then(data);
+};
 
-  {
-    type: "input",
-    message: "What is the engineers name?",
-    name: "engineerName",
-  },
-  {
-    type: "input",
-    message: "What is the engineers ID?",
-    name: "engineerID",
-  },
-  {
-    type: "input",
-    message: "What is the engineers github?",
-    name: "engineerGit",
-  },
+//if selected exit then exit the promts
 
-  // when we have finished entering this information for the engineer then return to the promts so we can add more teammates or exit.
+//if selected engineer is selected from choices then do the following
 
-  //if intern is selected from choices then do the following
+const enginInput = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the engineers name?",
+        name: "engineerName",
+      },
+      {
+        type: "input",
+        message: "What is the engineers ID?",
+        name: "engineerID",
+      },
+      {
+        type: "input",
+        message: "What is the engineers github?",
+        name: "engineerGit",
+      },
+    ])
+    .then(data);
+};
 
-  {
-    type: "input",
-    message: "What is the interns name?",
-    name: "internName",
-  },
-  {
-    type: "input",
-    message: "What is the interns ID?",
-    name: "internID",
-  },
-  {
-    type: "input",
-    message: "What is the interns email?",
-    name: "internEmail",
-  },
-  {
-    type: "input",
-    message: "What is the interns school name?",
-    name: "internSchool",
-  },
+// when we have finished entering this information for the engineer then return to the promts so we can add more teammates or exit.
 
-  // when we hav finished entering this information for the intern then return to the prompts so we can add more teammates or exit.
-]);
+//if intern is selected from choices then do the following
+
+const internInput = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the interns name?",
+        name: "internName",
+      },
+      {
+        type: "input",
+        message: "What is the interns ID?",
+        name: "internID",
+      },
+      {
+        type: "input",
+        message: "What is the interns email?",
+        name: "internEmail",
+      },
+      {
+        type: "input",
+        message: "What is the interns school name?",
+        name: "internSchool",
+      },
+    ])
+
+    // create intern object push into the array
+
+    .then((data) => {
+      const intern = new Intern(
+        data.internName,
+        data.internID,
+        data.internEmail,
+        data.internSchool
+      );
+    });
+};
+
+// when we hav finished entering this information for the intern then return to the prompts so we can add more teammates or exit.
